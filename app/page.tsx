c96bc9d5086542600f92b24b9ca3be12a19bf3d1
AuthnSaparachchi <authn.sapuarachchi@gmail.com>
2026-04-06 00:18:29 +0530

    clean the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,6 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? (
-              <EnvVarWarning />
-            ) : (
-              <Suspense>
-                <AuthButton />
-              </Suspense>
-            )}
-          </div>
-        </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
@@ -42,15 +25,15 @@ export default function Home() {
           <p>
             Powered by{" "}
             <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              href="https://sliitsesc.org/"
               target="_blank"
               className="font-bold hover:underline"
               rel="noreferrer"
             >
-              Supabase
+              SESC-SLIIT
             </a>
           </p>
-          <ThemeSwitcher />
+          
         </footer>
       </div>
     </main>
